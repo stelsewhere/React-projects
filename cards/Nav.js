de15d6e7781f362ba00fbf57fa48fdac75bc3bea
Liudmyla Kladko <86486525+stelsewhere@users.noreply.github.com>
2021-11-20 14:02:11 +0200

    Add files via upload

---
+++ cards/Nav.js
@@ -0,0 +1,9 @@
+
+export default function Nav() {
+    return (
+        <nav className="nav">
+            <img src=""></img>
+            <span>my travel journal.</span>
+        </nav>
+    );
+}
---
+++ cards/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        img: "",
+        icon: "",
+        country: "Japan",
+        link: "",
+        title: "Mount Fuji",
+        date: "12 Jan, 2021 - 24 Jan, 2021",
+        description: "Mount Fuji..."
+    },
+    {
+        img: "",
+        icon: "",
+        country: "Australia",
+        link: "",
+        title: "Sydney Opera House",
+        date: "27 May, 2021 - 8 Jun, 2021",
+        description: "Mount Fuji..."
+    },
+    {
+        img: "",
+        icon: "",
+        country: "Norway",
+        link: "",
+        title: "Geirangerfjord",
+        date: "01 Oct, 2021 - 18 Nov, 2021",
+        description: "Mount Fuji..."
+    }
+]
