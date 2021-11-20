7bea2cfdca86c0608e4c9af5d59e061c6bbd4071
Liudmyla Kladko <86486525+stelsewhere@users.noreply.github.com>
2021-11-20 14:01:04 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './App.css';
+import Main from "./Main";
+import Nav from "./Nav"
+
+export default function App() {
+  return (
+    <div className="container">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
