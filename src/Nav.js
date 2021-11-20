9b81f119b90ccba1a2cdf7cd0bf46151d64d4c7c
Liudmyla Kladko <86486525+stelsewhere@users.noreply.github.com>
2021-11-20 14:01:54 +0200

    Delete src directory

--- src/Nav.js
+++
@@ -1,9 +0,0 @@
-
-export default function Nav() {
-    return (
-        <nav className="nav">
-            <img src=""></img>
-            <span>my travel journal.</span>
-        </nav>
-    );
-}
